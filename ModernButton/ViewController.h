fe70b5de002e650b6e36c079b994100ab459e572
Gerald Hewes <ghewes@sessionm.com>
2014-08-07 11:49:47 -0400

    Simple Button App with Native Achievement

--- ModernButton/ViewController.h
+++ ModernButton/ViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SessionM.h"
+#import "SMPortalButton.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <SessionMDelegate> {
+        IBOutlet UIButton * bigRedButton;
+        IBOutlet UIButton * bigGreenButton;
+        IBOutlet UIButton * bigBlueButton;
+}
 
+@property (nonatomic, retain) UIButton * bigGreenButton;
+@property (nonatomic, retain) UIButton * bigRedButton;
+@property (nonatomic, retain) UIButton * bigBlueButton;
+
+- (IBAction)redButtonAction:(id)sender;
+- (IBAction)greenButtonAction:(id)sender;
+- (IBAction)blueButtonAction:(id)sender;
 
 @end
 
--- ModernButton/ViewController.m
+++ ModernButton/ViewController.m
@@ -7,21 +7,57 @@
 //
 
 #import "ViewController.h"
+#import "ButtonNativeAchievement.h"
 
 @interface ViewController ()
 
 @end
 
+#define YOUR_APP_ID @"401c5af1432b5c9a04f99d82a9821541ef88aa86"
+#define YOUR_TEST_EVENT @"shoot_bunny"
+
 @implementation ViewController
+
+@synthesize bigRedButton;
+@synthesize bigGreenButton;
+@synthesize bigBlueButton;
             
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Set the delegate so we get notified from the SDK
+    [[SessionM sharedInstance] setDelegate:self];
+    // Init the SDK
+    SMStart(YOUR_APP_ID);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)redButtonAction:(id)sender{
+     SMAction(YOUR_TEST_EVENT);
+}
+
+- (IBAction)greenButtonAction:(id)sender{
+    [[SessionM sharedInstance] presentActivity:SMActivityTypePortal];
+}
+
+- (IBAction)blueButtonAction:(id)sender{
+    
+    SMAchievementData *achievementData =
+    [SessionM sharedInstance].unclaimedAchievement;
+    
+    if (achievementData) {
+        ButtonNativeAchievement *activity = [[ButtonNativeAchievement alloc] initWithAchievmentData:achievementData];
+        [activity present];
+    }
+
+
+return;
 }
 
 @end
