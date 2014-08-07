3a095c603117133dcc25ea9114863a43d9efa75c
Gerald Hewes <ghewes@sessionm.com>
2014-08-06 22:55:07 -0400

    Initial Commit

---
+++ ModernButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModernButton
+//
+//  Created by Gerald Hewes on 8/6/14.
+//  Copyright (c) 2014 Gerald Hewes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModernButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ModernButton
+//
+//  Created by Gerald Hewes on 8/6/14.
+//  Copyright (c) 2014 Gerald Hewes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
